82a8dd272afc471f576e4c6da0b0988122fdcfe3
Fray Herrera <ufonefray@gmail.com>
2026-04-27 01:31:28 -0500

    a

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,27 @@
-const CACHE_NAME = 'v1.0.0';
+const CACHE_NAME = 'version-1';
+const assets = ['/pwa/icons/ios/192.png']; // Añade aquí tus rutas de assets
 
-const cacheAssets = [
-    '/favicon.ico',
-];
-
+// Evento de Instalación: Guarda en caché archivos estáticos
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            cache.addAll(cacheAssets);
-        })
+        caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
     );
 });
 
+// Evento de Activación: Limpia cachés antiguas
 self.addEventListener('activate', event => {
     event.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(keyList.map(key => {
-                if (key !== CACHE_NAME) {
-                    return caches.delete(key);
-                }
-            }));
-        })
+        caches.keys().then(keys => Promise.all(
+            keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+        ))
     );
 });
 
+// Evento Fetch: Intercepta peticiones y sirve desde caché si existe
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.match(event.request).then(response => {
-                return response || fetch(event.request);
-            });
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
         })
     );
 });
